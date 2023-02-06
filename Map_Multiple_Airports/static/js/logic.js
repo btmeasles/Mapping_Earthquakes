ff216eb9684ffd97b314bef872c920aa00d9b9cc
btmeasles <btmeasles@gmail.com>
2023-02-05 21:36:30 -0700

    Added raw Multiple_Airports Data

--- Map_Multiple_Airports/static/js/logic.js
+++ Map_Multiple_Airports/static/js/logic.js
@@ -1,24 +1,10 @@
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);// 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"14",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2); 
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/btmeasles/Mapping_Earthquakes/main/majorAirports.json";
+
+
 // Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
     onEachFeature: function(feature, layer) {
