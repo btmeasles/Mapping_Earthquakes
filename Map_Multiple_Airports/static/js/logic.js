10e80c8f5a9acb0cdb2c484dfdc475d97a49570a
btmeasles <btmeasles@gmail.com>
2023-02-05 22:35:18 -0700

    Up to 12.5.4

--- Map_Multiple_Airports/static/js/logic.js
+++ Map_Multiple_Airports/static/js/logic.js
@@ -1,22 +1,34 @@
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2); 
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/btmeasles/Mapping_Earthquakes/main/majorAirports.json";
-
-
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup(`Airport Code: ${feature.properties.id}<hr class = "rounded">Airport Name: ${feature.properties.name}`);
-    }
-}).addTo(map);
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/btmeasles/Mapping_Earthquakes/main/majorAirports.json";
+
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup(
+            `ID: ${feature.properties.id}<hr class=rounded>Airport Name: ${feature.properties.name}`)}})
+    .addTo(map)
+    });
+
