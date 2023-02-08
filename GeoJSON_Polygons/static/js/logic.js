67aa7ea71587c6a4efb950a32a5d609c238acccd
btmeasles <btmeasles@gmail.com>
2023-02-07 20:10:08 -0700

    Finished 12.5

--- GeoJSON_Polygons/static/js/logic.js
+++ GeoJSON_Polygons/static/js/logic.js
@@ -1,44 +1,47 @@
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        style_id: "dark-v10",
+        style_id: "streets-v11",
         accessToken: API_KEY
 });
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        style_id: "light-v10",
+        style_id: "satellite-streets-v11",
         accessToken: API_KEY
 });
 
 var baseMaps = {
-    Light: light,
-    Dark: dark
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
 };
 
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
-    zoom: 2,
-    layers: [dark]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 });
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/btmeasles/Mapping_Earthquakes/GeoJSON_Linestrings/GeoJSON_Linestrings/torontoRoutes.json"
+let torontoHoods = "https://raw.githubusercontent.com/btmeasles/Mapping_Earthquakes/GeoJSON_Polygons/GeoJSON_Polygons/torontoNeighborhoods.json"
+
+let polyStyle = {
+    color: 'blue',
+    fillColor: '#FF0000',
+    weight: 1
+}
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     onEachFeature: function(feature, layer) {
-      layer.bindPopup(
-        `Airline: ${feature.properties.airline}<hr class=rounded>Destination: ${feature.properties.dst}`)
-    }}).setStyle(
-    {
-    color: "#FFFF66",
-    weight: 2
-    }).addTo(map);
+        style: polyStyle,
+        layer.bindPopup(
+            `<h3>Neighborhood: ${feature.properties.AREA_NAME}</h3>`)
+    }}).addTo(map);
 });
 L.control.layers(baseMaps).addTo(map)
 
