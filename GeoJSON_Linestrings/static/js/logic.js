155a5c166c19402d690556e341ef9edf3d5af6fc
btmeasles <btmeasles@gmail.com>
2023-02-07 18:54:07 -0700

    Finished GeoJSON Line plotting

--- GeoJSON_Linestrings/static/js/logic.js
+++ GeoJSON_Linestrings/static/js/logic.js
@@ -1,14 +1,14 @@
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        style_id: "mapbox/dark-v11",
+        style_id: "dark-v10",
         accessToken: API_KEY
 });
 
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        style_id: "mapbox/light-v11",
+        style_id: "light-v10",
         accessToken: API_KEY
 });
 
@@ -17,11 +17,28 @@ var baseMaps = {
     Dark: dark
 };
 
-let map = L.map('map_id', {
+let map = L.map('mapid', {
     center: [44.0, -80.0],
     zoom: 2,
-    layers: [light]
+    layers: [dark]
 });
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData
+let torontoData = "https://raw.githubusercontent.com/btmeasles/Mapping_Earthquakes/GeoJSON_Linestrings/GeoJSON_Linestrings/torontoRoutes.json"
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(
+        `Airline: ${feature.properties.airline}<hr class=rounded>Destination: ${feature.properties.dst}`)
+    }}).setStyle(
+    {
+    color: "#FFFF66",
+    weight: 2
+    }).addTo(map);
+});
+L.control.layers(baseMaps).addTo(map)
+
